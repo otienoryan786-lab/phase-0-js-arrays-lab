0c77c43300ecbd5559a87e210e127f3d475e3b3a
otienoryan786-lab <otienoryan786@gmail.com>
2026-09-24 17:42:33 +0300

    git commit -m "Implement product inventory management functions"

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,26 @@
 // Write your code here
+// Create the Product Inventory Array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+// Access Product Information: log the first product in the array
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+// Add a new product to the end of the array
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// Update Product Information: change the name of a product at a given index
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+// Remove a Product: remove the last product from the array
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
